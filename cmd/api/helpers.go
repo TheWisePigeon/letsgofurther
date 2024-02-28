21aa191e28d258deda4801bdc5430d3e232f5581
TheWisePigeon <zozozozeph@gmail.com>
2024-02-28 23:06:41 +0000

    json encoding

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -14,4 +15,19 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, fmt.Errorf("Error while reading id from params: %w", err)
 	}
 	return movieID, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	jsonData = append(jsonData, '\n')
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+  w.Write(jsonData)
+	return nil
 }
