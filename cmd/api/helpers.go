c1ae81e8fbda8ab6c9c50453b3036c82363b04e4
TheWisePigeon <zozozozeph@gmail.com>
2024-03-03 11:19:33 +0000

    more json decoding error handling

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,11 +7,16 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 type envelope map[string]interface{}
+
+const (
+	MAX_BYTES = 1_048_576
+)
 
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
@@ -38,7 +43,10 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, destination interface{}) error {
-	err := json.NewDecoder(r.Body).Decode(destination)
+	http.MaxBytesReader(w, r.Body, int64(MAX_BYTES))
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	err := decoder.Decode(destination)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshallTypeError *json.UnmarshalTypeError
@@ -55,12 +63,24 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, destina
 			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshallTypeError.Offset)
 		case errors.Is(err, io.EOF):
 			return fmt.Errorf("body must not be empty")
+		case strings.HasPrefix(err.Error(), "json: unkown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unkown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", MAX_BYTES)
 		case errors.As(err, &invalidUnmarshallError):
 			panic(err)
 		default:
 			return err
 		}
-
 	}
+  err = decoder.Decode(&struct{}{})
+  if err != io.EOF {
+    return errors.New("body must only contain a single JSON object")
+  }
 	return nil
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
